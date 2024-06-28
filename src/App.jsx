7223ed6059e5e6f974cf691ce7ba665806a561fd
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-06-28 23:38:02 +0530

    added Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth/signup" element={<Signup />} />
           <Route path="/auth/login" element={<Login />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </>
     </div>
