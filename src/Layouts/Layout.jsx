51c2cf5121ae73dbf89db4d66ecbfa0db9a4385c
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-06-30 13:02:40 +0530

    add navigator to home page

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import Pizzalogo from "../assets/images/pizza1.png";
 import Footer from "../components/Footer";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../Redux/Slices/AuthSlice";
 function Layout({ children }) {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
@@ -13,10 +13,13 @@ function Layout({ children }) {
     e.preventDefault();
     dispatch(logout());
   }
+  const navigate = useNavigate();
   return (
     <div>
       <nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center"
+          onClick={() => navigate("/")}
+        >
           <p>Pizza App</p>
           <img src={Pizzalogo} alt="Pizza logo" />
         </div>
