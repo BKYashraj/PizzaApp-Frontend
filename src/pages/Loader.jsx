4a80de5ea9b6faa9098139fc239b23e1d6d31376
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-21 11:17:48 +0530

    done

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -1,9 +1,9 @@
-import { Container} from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 
 const Loader = ({show}) => {
   return show && (
     <Container className='text-center p-5 text-black'>
-      <h1>Loading....</h1>
+      <Spinner size='lg'/>
     </Container>
   )
 };
