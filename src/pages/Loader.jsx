7dd34f5d318bd37559ed6d379d731bf9097314e1
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-21 11:16:23 +0530

    done

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -2,7 +2,7 @@ import { Container} from "react-bootstrap";
 
 const Loader = ({show}) => {
   return show && (
-    <Container className='text-center p-5'>
+    <Container className='text-center p-5 text-black'>
       <h1>Loading....</h1>
     </Container>
   )
